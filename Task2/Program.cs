543ab6fb291186435b0e0f328781d565998f04cc
Asanovich <asansvetlana@yandex.ru>
2023-02-02 13:19:16 +0300

    Нахождение максимального и минимального чисел

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = b;
+if (a > max) max = a;
+if (b > max) max = b;
+if (a < max) min = a;
+if (b < max) min = b;
+ 
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
