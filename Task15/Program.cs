bc9ccab0b7c379eb5f4e36dc929cfd36f231e267
Asanovich <asansvetlana@yandex.ru>
2023-02-08 18:39:59 +0300

    Определение по номеру дня выходной он или нет

---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n <= 7)
+{
+    if (n <= 5)
+        Console.WriteLine("нет");
+    else
+        Console.WriteLine("да");
+        break;
+}
